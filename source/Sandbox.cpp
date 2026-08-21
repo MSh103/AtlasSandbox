e75a079855ee952e40df6ea9f7c40610849e5098
Mohammad Shaikh <mohammad@oshaikh.sa>
2026-08-21 22:22:33 +0300

    Create & Implement Transformations & MVP

--- source/Sandbox.cpp
+++ source/Sandbox.cpp
@@ -27,12 +27,12 @@ void Sandbox::OnAttach()
 	m_VAO = std::make_unique<Atlas::VertexArray>();
 	m_Shader = std::make_shared<Atlas::Shader>();
 
-	//m_Shader->LoadFromFile(Atlas::ShaderType::Vertex, "assets/Vertex.glsl");
-	//m_Shader->LoadFromFile(Atlas::ShaderType::Fragment, "assets/Fragment.glsl");
-	m_Shader->LoadFromFile(Atlas::ShaderType::Basic, "assets/Basic.glsl");
+	m_Shader->LoadFromFile(Atlas::ShaderType::Vertex, "assets/Vertex.glsl");
+	m_Shader->LoadFromFile(Atlas::ShaderType::Fragment, "assets/Fragment.glsl");
+	//m_Shader->LoadFromFile(Atlas::ShaderType::Basic, "assets/Basic.glsl");
 	m_Shader->LinkObjectsToProgram();
 
-	std::shared_ptr<Atlas::VertexBuffer> VBO = std::make_shared<Atlas::VertexBuffer>(&vertices, sizeof(Vertex) * 4);
+	std::shared_ptr<Atlas::VertexBuffer> VBO = std::make_shared<Atlas::VertexBuffer>(vertices, sizeof(Vertex) * 4);
 	VBO->Use();
 	VBO->SetLayout({
 		{Atlas::ShaderDataType::Float3, "a_Position"},
@@ -44,6 +44,19 @@ void Sandbox::OnAttach()
 	std::shared_ptr<Atlas::IndexBuffer> IBO = std::make_shared<Atlas::IndexBuffer>(indices, 6);
 	IBO->Use();
 	m_VAO->SetIndexBuffer(IBO);
+
+	float width = Atlas::Application::Get().GetWindow()->GetWidth();
+	float height = Atlas::Application::Get().GetWindow()->GetHeight();
+
+	m_Transform.Position = { 0.f, 0.f, 0.f };
+	m_Transform.Rotation = { 0.f, 45.f, 45.f };
+	m_Transform.Scale = { 1.f, 1.f, 1.f };
+
+	m_Model = glm::mat4(1.0f);
+	m_View = glm::mat4(1.0f);
+	m_Projection = glm::mat4(1.0f);
+	m_Projection = glm::perspective(glm::radians(45.f), width / height, 0.1f, 100.f);
+	m_View = glm::translate(m_View, glm::vec3(0.f, 0.f, -3.0f));
 }
 
 void Sandbox::OnDetach()
@@ -56,6 +69,15 @@ void Sandbox::OnEvent(Atlas::Event& e)
 
 void Sandbox::OnUpdate(Atlas::TimeStep ts)
 {
+	//m_Transform.Rotation.x += 1.5f * ts;
+	m_Transform.Rotation.y += 1.5f * ts;
+	//m_Transform.Rotation.z += 1.5f * ts;
+
+	m_Model = glm::translate(glm::mat4(1.0f), m_Transform.Position) * 
+		  glm::rotate(glm::mat4(1.0f), m_Transform.Rotation.x, glm::vec3(1.f, 0.f, 0.f)) *
+		  glm::rotate(glm::mat4(1.0f), m_Transform.Rotation.y, glm::vec3(0.f, 1.f, 0.f)) *
+		  glm::rotate(glm::mat4(1.0f), m_Transform.Rotation.z, glm::vec3(0.f, 0.f, 1.f)) *
+		  glm::scale(glm::mat4(1.0f), m_Transform.Scale);
 }
 
 void Sandbox::OnRender()
@@ -65,6 +87,9 @@ void Sandbox::OnRender()
 
 	m_VAO->Use();
 	m_Shader->Use();
+	m_Shader->Upload("u_Model", m_Model);
+	m_Shader->Upload("u_View", m_View);
+	m_Shader->Upload("u_Projection", m_Projection);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (const void*)0);
 }
 
@@ -72,5 +97,10 @@ void Sandbox::OnImGuiRender()
 {
 	ImGui::Begin("Debug");
 	ImGui::ColorEdit3("Clear Screen", glm::value_ptr(m_Color));
+	ImGui::Separator();
+	ImGui::Text("Transform");
+	ImGui::DragFloat3("Position",	&m_Transform.Position.x, 0.1f);
+	ImGui::DragFloat3("Rotation",	&m_Transform.Rotation.x, 0.1f);
+	ImGui::DragFloat3("Scale",	&m_Transform.Scale.x, 0.1f);
 	ImGui::End();
 }
--- source/Sandbox.h
+++ source/Sandbox.h
@@ -3,6 +3,13 @@
 #include <Atlas.h>
 #include <Atlas/Renderer.h>
 
+struct Transform
+{
+	glm::vec3 Position;
+	glm::vec3 Rotation;
+	glm::vec3 Scale;
+};
+
 class Sandbox : public Atlas::Layer
 {
 public:
@@ -19,6 +26,8 @@ public:
 
 private:
 	glm::vec3 m_Color;
+	Transform m_Transform;
+	glm::mat4 m_Model, m_View, m_Projection;
 
 	std::unique_ptr<Atlas::VertexArray> m_VAO;
 	std::shared_ptr<Atlas::Shader> m_Shader;
